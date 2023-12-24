73f73d40d3cb89eedc78e0e8bb1f727707999f34
Aellun <okello.k.otieono@gmail.com>
2023-12-23 17:00:59 -0800

    test cases

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
-""" """
+"""review class odule unit test """
 from tests.test_models.test_base_model import test_basemodel
 from models.review import Review
 
 
 class test_review(test_basemodel):
-    """ """
+    """reviewclass test cases """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ review class instance initialization"""
         super().__init__(*args, **kwargs)
         self.name = "Review"
         self.value = Review
 
     def test_place_id(self):
-        """ """
+        """ id test"""
         new = self.value()
         self.assertEqual(type(new.place_id), str)
 
     def test_user_id(self):
-        """ """
+        """ user_id"""
         new = self.value()
         self.assertEqual(type(new.user_id), str)
 
     def test_text(self):
-        """ """
+        """ text attribute"""
         new = self.value()
         self.assertEqual(type(new.text), str)
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,34 +1,34 @@
 #!/usr/bin/python3
-""" """
+"""unit test for the class user module """
 from tests.test_models.test_base_model import test_basemodel
 from models.user import User
 
 
 class test_User(test_basemodel):
-    """ """
+    """ test cases for the class user"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ instances initilization"""
         super().__init__(*args, **kwargs)
         self.name = "User"
         self.value = User
 
     def test_first_name(self):
-        """ """
+        """ first name"""
         new = self.value()
         self.assertEqual(type(new.first_name), str)
 
     def test_last_name(self):
-        """ """
+        """ last name"""
         new = self.value()
         self.assertEqual(type(new.last_name), str)
 
     def test_email(self):
-        """ """
+        """ email"""
         new = self.value()
         self.assertEqual(type(new.email), str)
 
     def test_password(self):
-        """ """
+        """ password"""
         new = self.value()
         self.assertEqual(type(new.password), str)
