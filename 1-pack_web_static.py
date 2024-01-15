372d62616705ba7485860e84a9d3d031e495bf6f
Aellun <okello.k.otieono@gmail.com>
2024-01-15 13:49:50 -0800

    updated

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,22 +1,32 @@
 #!/usr/bin/python3
-from fabric.api import local
-from datetime import date
-from time import strftime
+"""
+Fabric script that generates a .tgz archive from the contents
+of the web_static folder of the AirBnB Clone repo
+"""
+import os
+from datetime import datetime
+from fabric.api import local, runs_once
 
 
-""" Fabric script that generates a .tgz archive from the contents
-of the web_static folder of the AirBnB Clone repo"""
-
-
+@runs_once
 def do_pack():
-
-    filename = strftime("%Y%m%d%H%M%S")
+    """Archives the static files."""
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    d_time = datetime.now()
+    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        d_time.year,
+        d_time.month,
+        d_time.day,
+        d_time.hour,
+        d_time.minute,
+        d_time.second
+    )
     try:
-        local("mkdir -p versions")
-        local("tar -czvf versions/web_static_{}.tgz web_static/"
-              .format(filename))
-
-        return "versions/web_static_{}.tgz".format(filename)
-
-    except Exception as e:
-        return None
+        print("Packing web_static to {}".format(output))
+        local("tar -cvzf {} web_static".format(output))
+        size = os.stat(output).st_size
+        print("web_static packed: {} -> {} Bytes".format(output, size))
+    except Exception:
+        output = None
+    return output
