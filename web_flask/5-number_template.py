0e89bee812c60acc1292b872df2e2db6dd84b2b6
Aellun <okeelo.k.otieno@gmail.com>
2024-02-23 01:28:25 +0300

    Number template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,35 +1,33 @@
 #!/usr/bin/python3
-"""This script starts flask web application with two
-main routings and 3 others"""
+"""This script starts flask web application with six routings"""
 
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    """Return string when the  route is queried"""
+    """Returns string when the  route is queried"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb')
 def hbnb():
-    """Return string when the route is queried"""
+    """Returns string when the route is queried"""
     return 'HBNB'
 
 
 @app.route('/c/<text>')
 def c_is_fun(text):
-    """Return predifined reformatted text"""
+    """Returns predefined reformatted text
+    """
     return 'C ' + text.replace('_', ' ')
 
 
 @app.route('/python/')
 @app.route('/python/<text>')
 def python_with_text(text='is cool'):
-    """Reformat text based on optional variable
-    predefined text = cool"""
+    """Reformat text based on optional variable predefined text=cool"""
     return 'Python ' + text.replace('_', ' ')
 
 
@@ -37,6 +35,13 @@ def python_with_text(text='is cool'):
 def number(n=None):
     """Allow request if path variable is a valid integer"""
     return str(n) + ' is a number'
+
+
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    """Retrieve the html template for request"""
+    path = '5-number.html'
+    return render_template(path, n=n)
 
 
 if __name__ == '__main__':
